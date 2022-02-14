37581ce0c9e593498aa5c57f8aa05839255d80c2
chlee1001 <chlee1001@naver.com>
2022-02-15 00:16:44 +0900

    :sparkles: 페이지네이션 페이징 처리

--- js-simple-project/js-todo-list/practice/script.js
+++ js-simple-project/js-todo-list/practice/script.js
@@ -9,9 +9,57 @@
     const $todos = get('.todos');
     const $form = get('.todo_form');
     const $todoInput = get('.todo_input');
+    const $pagination = get('.pagination');
 
+    const totalCount = 53;
+    const pageCount = 5
     const limit = 5;
     let currentPage = 1;
+
+    const pagination = () => {
+        let totalPage = Math.ceil(totalCount / limit);
+        let pageGroup = Math.ceil(currentPage / pageCount);
+
+        let lastNumber = pageGroup * pageCount;
+        if (lastNumber > totalPage) {
+            lastNumber = totalPage;
+        }
+        let firstNumber = lastNumber - (pageCount - 1);
+
+        const next = lastNumber + 1;
+        const prev = firstNumber - 1;
+
+        let html = '';
+
+        if (prev > 0) {
+            html += "<button class='prev' data-fn='prev'>이전</button> "
+        }
+        for (let i = firstNumber; i <= lastNumber; i++) {
+            html += `<button class="pageNumber" id="page_${i}">${i}</button>`
+        }
+        if (lastNumber < totalPage) {
+            html += `<button class='next' data-fn='next'>다음</button>`
+        }
+        $pagination.innerHTML = html;
+
+        const $currentPageNumber = get(`.pageNumber#page_${currentPage}`);
+        $currentPageNumber.style.color = '#9dc0e9';
+
+        const $currentPageNumbers = document.querySelectorAll('.pagination button');
+        $currentPageNumbers.forEach(button => {
+            button.addEventListener('click', () => {
+                if (button.dataset.fn === 'prev') {
+                    currentPage = prev;
+                } else if (button.dataset.fn === 'next') {
+                    currentPage = next;
+                } else {
+                    currentPage = button.innerText;
+                }
+                pagination();
+                getTodos();
+            })
+        })
+    }
 
     const createTodoElement = (item) => {
         const {id, content, completed} = item
@@ -172,6 +220,7 @@
     const init = () => {
         window.addEventListener('DOMContentLoaded', () => { // HTML전부 불러왔을 때 getTodos함수 실행
             getTodos();
+            pagination();
         })
         $form.addEventListener('submit', addTodo);
         $todos.addEventListener('click', toggleTodo);
