1f3816333246852ffbd8df1a08518b56e138bfad
chlee1001 <chlee1001@naver.com>
2022-02-14 23:58:52 +0900

    :sparkles: 페이지네이션 데이터 가져오기

--- js-simple-project/js-todo-list/practice/script.js
+++ js-simple-project/js-todo-list/practice/script.js
@@ -10,6 +10,9 @@
     const $form = get('.todo_form');
     const $todoInput = get('.todo_input');
 
+    const limit = 5;
+    let currentPage = 1;
+
     const createTodoElement = (item) => {
         const {id, content, completed} = item
         const $todoItem = document.createElement('div')
@@ -55,7 +58,7 @@
     }
 
     const getTodos = () => {
-        fetch(API_URL)
+        fetch(`${API_URL}?_page=${currentPage}&_limit=${limit}`)
             .then(response => response.json())
             .then(
                 todos => renderAllTodos(todos))
