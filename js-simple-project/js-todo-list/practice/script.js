8ee85ac862f5cbb0e79745dd0b954991b0392997
chlee1001 <chlee1001@naver.com>
2022-02-14 23:27:46 +0900

    :sparkles: API연동- 데이터 수정하기

--- js-simple-project/js-todo-list/practice/script.js
+++ js-simple-project/js-todo-list/practice/script.js
@@ -100,6 +100,55 @@
         )
             .then(getTodos)
             .catch(e => console.error(e))
+    }
+
+    const changeEditMode = (e) => {
+        const $item = e.target.closest(('.item'));
+        const $label = $item.querySelector('label');
+        const $editInput = $item.querySelector('input[type="text"]')
+        const $contentButtons = $item.querySelector('.content_buttons');
+        const $editButtons = $item.querySelector('.edit_buttons');
+        const tempValue = $editInput.value;
+
+        if (e.target.className === 'todo_edit_button') {
+            $label.style.display = 'none';
+            $editInput.style.display = 'block';
+            $contentButtons.style.display = 'none';
+            $editButtons.style.display = 'block';
+            $editInput.focus();
+            $editInput.value = ''
+            $editInput.value = tempValue
+        }
+
+        if (e.target.className === 'todo_edit_cancel_button') {
+            $label.style.display = 'block';
+            $editInput.style.display = 'none';
+            $contentButtons.style.display = 'block';
+            $editButtons.style.display = 'none';
+            $editInput.value = $label.innerText;
+        }
+    }
+
+    const editTodo = (e) => {
+        if (e.target.className !== 'todo_edit_confirm_button') {
+            return;
+        }
+        const $item = e.target.closest(('.item'));
+        const id = $item.dataset.id;
+        const editInput = $item.querySelector('input[type="text"]');
+        const content = editInput.value;
+
+        fetch(`${API_URL}/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({content}),
+            }
+        )
+            .then(getTodos)
+            .catch(e => console.error(e))
+
 
     }
 
@@ -109,6 +158,8 @@
         })
         $form.addEventListener('submit', addTodo);
         $todos.addEventListener('click', toggleTodo);
+        $todos.addEventListener('click', changeEditMode);
+        $todos.addEventListener('click', editTodo);
     }
     init()
 })()
