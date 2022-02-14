b04b9fbcbab8e922b26699a5ef96a88c0d63df30
chlee1001 <chlee1001@naver.com>
2022-02-14 23:13:48 +0900

    :sparkles: API연동- 데이터 체크박스 처리하기

--- js-simple-project/js-todo-list/practice/script.js
+++ js-simple-project/js-todo-list/practice/script.js
@@ -11,8 +11,9 @@
     const $todoInput = get('.todo_input');
 
     const createTodoElement = (item) => {
-        const {id, content} = item
+        const {id, content, completed} = item
         const $todoItem = document.createElement('div')
+        const isChecked = completed ? 'checked' : ''
         $todoItem.classList.add('item')
         $todoItem.dataset.id = id
         $todoItem.innerHTML = `
@@ -20,6 +21,7 @@
               <input
                 type="checkbox"
                 class='todo_checkbox' 
+                ${isChecked}
               />
               <label>${content}</label>
               <input type="text" value="${content}" />
@@ -69,7 +71,7 @@
             completed: false,
         };
         fetch(API_URL, {
-            method: 'post',
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -80,11 +82,33 @@
         }).catch(e => console.error(e));
     }
 
+    const toggleTodo = (e) => {
+        if (e.target.className !== 'todo_checkbox') {
+            return;
+        }
+        const $item = e.target.closest('.item');
+        const id = $item.dataset.id;
+        const completed = e.target.checked;
+
+        fetch(`${API_URL}/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({completed}),
+            }
+        )
+            .then(getTodos)
+            .catch(e => console.error(e))
+
+    }
+
     const init = () => {
         window.addEventListener('DOMContentLoaded', () => { // HTML전부 불러왔을 때 getTodos함수 실행
             getTodos();
         })
         $form.addEventListener('submit', addTodo);
+        $todos.addEventListener('click', toggleTodo);
     }
     init()
 })()
