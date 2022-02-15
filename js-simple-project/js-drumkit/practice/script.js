d27f44d5a774e1d3839863729f1d5052f2aeda42
chlee1001 <chlee1001@naver.com>
2022-02-15 23:35:30 +0900

    :tada:드럼킷 project init

---
+++ js-simple-project/js-drumkit/practice/script.js
@@ -0,0 +1,24 @@
+;(function () {
+  'use strict'
+
+  const get = function (target) {
+    return document.querySelector(target)
+  }
+
+  const getAll = function (target) {
+    return document.querySelectorAll(target)
+  }
+
+  const soundsRoot = 'assets/sounds/'
+  const drumSounds = [
+    { key: 81, sound: 'clap.wav' },
+    { key: 87, sound: 'crash.wav' },
+    { key: 69, sound: 'hihat.wav' },
+    { key: 65, sound: 'kick.wav' },
+    { key: 83, sound: 'openhat.wav' },
+    { key: 68, sound: 'ride.wav' },
+    { key: 90, sound: 'shaker.wav' },
+    { key: 88, sound: 'snare.wav' },
+    { key: 67, sound: 'tom.wav' },
+  ]
+})()
