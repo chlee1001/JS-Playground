2dacb49476f46e96626b772a6b04d706dd523d35
chlee1001 <chlee1001@naver.com>
2022-02-17 23:28:00 +0900

    :sparkles: 간단 검색 구현

--- js-simple-project/js-youtube/practice/script.js
+++ js-simple-project/js-youtube/practice/script.js
@@ -1,3 +1,32 @@
 ;(function () {
   'use strict'
+
+  const get = (target) => document.querySelector(target)
+  const getAll = (target) => document.querySelectorAll(target)
+
+  const $search = get('#search')
+  const $list = getAll('.contents.list figure')
+  const $searchButton = get('.btn_search')
+
+  const search = () => {
+    let searchText = $search.value.toLowerCase()
+
+    for (let index = 0; index < $list.length; index++) {
+      const $target = $list[index].querySelector('strong')
+      const text = $target.textContent.toLowerCase()
+      if (-1 < text.indexOf(searchText)) {
+        $list[index].style.display = 'flex'
+      } else {
+        $list[index].style.display = 'none'
+      }
+    }
+  }
+
+  const init = () => {
+    $search.addEventListener('keyup', search)
+    $searchButton.addEventListener('click', search)
+  }
+
+  init()
+
 })()
