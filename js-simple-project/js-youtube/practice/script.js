55eda893fe336b74869b170b0a926ab47db0b17e
chlee1001 <chlee1001@naver.com>
2022-02-17 23:39:35 +0900

    :sparkles: 뷰 페이지 진입 시 uri hash사용

--- js-simple-project/js-youtube/practice/script.js
+++ js-simple-project/js-youtube/practice/script.js
@@ -32,6 +32,27 @@
     webPlay.setAttribute('srcset', './assets/sample.jpg')
   }
 
+  const getViewPage = () => {
+    const viewTitle = get('.view strong')
+    viewTitle.innerText = decodeURI(window.location.hash.split('&')[1])
+
+    get('.list').style.display = 'none'
+    get('.view').style.display = 'flex'
+
+  }
+  const getListPage = () => {
+    get('.view').style.display = 'none'
+    get('.list').style.display = 'flex'
+  }
+
+  const hashChange = (e) => {
+    e.preventDefault()
+    const parentNode = e.target.closest('figure')
+    const viewTitle = parentNode.querySelector('strong').textContent
+    window.location.hash = `view${viewTitle}`
+    getViewPage()
+  }
+
   const init = () => {
     $search.addEventListener('keyup', search)
     $searchButton.addEventListener('click', search)
@@ -41,6 +62,18 @@
       $target.addEventListener('mouseover', onMouseOver)
       $target.addEventListener('mouseout', onMouseOut)
     }
+    for (let index = 0; index < $list.length; index++) {
+      $list[index].addEventListener('click', hashChange)
+    }
+
+    window.addEventListener('hashchange', () => {
+      const isView = -1 < window.location.hash.indexOf('view')
+      if (isView) {
+        getViewPage()
+      } else {
+        getListPage()
+      }
+    })
   }
 
   init()
