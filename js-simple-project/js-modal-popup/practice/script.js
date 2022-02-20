040c190c8c4ab544327d1f37063e48748f74e010
chlee1001 <chlee1001@naver.com>
2022-02-21 00:21:02 +0900

    :sparkles: 모달팝업 버튼 이벤트 추가

--- js-simple-project/js-modal-popup/practice/script.js
+++ js-simple-project/js-modal-popup/practice/script.js
@@ -2,4 +2,35 @@
   'use strict'
   const get = (target) => document.querySelector(target)
 
+  const $button = get('.modal_open_button')
+  const $modal = get('.modal')
+  const $body = get('body')
+  const $modalCancelButton = get('.modal_button.cancel')
+  const $modalConfirmButton = get('.modal_button.confirm')
+
+  const toggleModal = () => {
+    $modal.classList.toggle('show')
+    $body.classList.toggle('scroll_lock')
+  }
+
+  $button.addEventListener('click', () => {
+    toggleModal()
+  })
+
+  $modalCancelButton.addEventListener('click', () => {
+    toggleModal()
+    console.log('Cancel')
+  })
+
+  $modalConfirmButton.addEventListener('click', () => {
+    toggleModal()
+    console.log('Confirm')
+  })
+
+  window.addEventListener('click', e => {
+    if (e.target === $modal) {
+      toggleModal()
+      console.log('Cancel')
+    }
+  })
 })()
