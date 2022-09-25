b60b53014fb5bcd25fc9f2dffe21e5ba1204be2a
chlee1001 <chlee1001@naver.com>
2022-09-25 23:15:31 +0900

    :sparkles: 데이터 페치 후 페이지네이션 적용

--- react-pagination/src/pages/homePage/index.tsx
+++ react-pagination/src/pages/homePage/index.tsx
@@ -1,9 +1,73 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import Pagination from '@components/Pagination';
+
+interface Airline {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  slogan: string;
+  head_quaters: string;
+  website: string;
+  established: string;
+}
+
+interface Passenger {
+  _id: string;
+  name: string;
+  trips: number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Response {
+  totalPassengers: number;
+  totalPages: number;
+  data: Array<Passenger>;
+}
 
 const Home: FC = () => {
+  const [page, setPage] = useState(0);
+  const [dataTotalPage, setDataTotalPage] = useState(0);
+  const [items, setItems] = useState<Array<Passenger>>([]);
+
+  const handlePageChange = (currentPage: number): void => {
+    setPage(currentPage);
+  };
+
+  useEffect(() => {
+    const fetch = async () => {
+      const params = { page, size: 10 };
+      const {
+        data: { totalPages, data },
+      } = await axios.get<Response>(
+        'https://api.instantwebtools.net/v1/passenger',
+        { params },
+      );
+
+      setDataTotalPage(totalPages);
+      setItems(data);
+    };
+
+    fetch();
+  }, [page]);
+
   return (
     <div>
-      <p>React Pagination</p>
+      <ul>
+        {items.map((item) => (
+          // eslint-disable-next-line no-underscore-dangle
+          <li key={item._id}>{item.name}</li>
+        ))}
+      </ul>
+      <Pagination
+        count={dataTotalPage}
+        onPageChange={handlePageChange}
+        page={page}
+      />
     </div>
   );
 };
