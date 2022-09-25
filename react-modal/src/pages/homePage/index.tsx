eaa1945dcdb475626dbfee2ad9dfe2c4e7bb0ea1
chlee1001 <chlee1001@naver.com>
2022-09-25 23:29:01 +0900

    :fire: 불필요한 파일 제거

--- react-modal/src/pages/homePage/index.tsx
+++ react-modal/src/pages/homePage/index.tsx
@@ -1,11 +1,9 @@
 import React, { FC } from 'react';
 
-import styles from './home-page.module.css';
-
 const Home: FC = () => {
   return (
-    <div className={styles.container}>
-      <p>Chaehyeon&apos;s React Typescript Boilerplate</p>
+    <div>
+      <p>Chaehyeon&apos;s React Typescript Modal</p>
     </div>
   );
 };
